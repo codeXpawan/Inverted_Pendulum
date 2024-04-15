cf555f080604b1b64951c5f753f0503d73a3631a
codeXpawan <shahpawan56@gmail.com>
2024-04-15 13:16:53 +0545

    closed loop control mechanism

---
+++ ROS/src/robot_control/robot_control/robot_control.py
@@ -0,0 +1,71 @@
+import rclpy
+from sensor_msgs.msg import Imu
+from geometry_msgs.msg import Twist
+from ament_index_python.packages import get_package_share_path
+import numpy as np
+
+class CmdVelSubscriberNode:
+
+    def __init__(self):
+        self.node = rclpy.create_node('robot_control')
+        self.subscription = self.node.create_subscription(
+            Imu,
+            '/imu',
+            self.control_callback,
+            10)
+        self.velocity_pub = self.node.create_publisher(Twist,'/cmd_vel',10)
+        self.K_p = 32
+        self.K_i = 1
+        self.K_d = 0.0
+        self.previous_error = 0
+        self.previous_sum_error = 0
+        
+        
+        
+    def quaternion_to_euler(self,q_w,q_x,q_y,q_z):
+        # Convert quaternion to Euler angles
+        roll = np.arctan2(2 * (q_w * q_x + q_y * q_z), 1 - 2 * (q_x**2 + q_y**2))
+        pitch = np.arcsin(2 * (q_w * q_y - q_z * q_x))
+        yaw = np.arctan2(2 * (q_w * q_z + q_x * q_y), 1 - 2 * (q_y**2 + q_z**2))
+        
+        return (roll,pitch,yaw)
+        
+    def control_callback(self, msg:Imu):
+        roll,pitch,yaw = self.quaternion_to_euler(msg.orientation.w,msg.orientation.x,msg.orientation.y,msg.orientation.z)
+        #one big issue is faced that is due to the 
+        #roll is the inclined angle of the robot
+        # linear_velocity = self.compute_pid(roll,0.0)
+        velocity = Twist()
+        velocity.linear.x = self.compute_pid(roll,0.0)
+        self.velocity_pub.publish(velocity)
+        
+    def compute_pid(self,observed_value,setpoint):
+        error = observed_value - setpoint
+        p_gain = self.K_p * error
+        self.previous_sum_error += error
+        i_gain = self.K_i * self.previous_sum_error
+        diff = self.previous_error - error
+        self.previous_error = error
+        d_gain = self.K_d * diff
+        
+        return p_gain+i_gain+d_gain
+        
+
+    def spin(self):
+        rclpy.spin(self.node)
+
+    def destroy_node(self):
+        self.file_handle.close()
+        self.node.destroy_node()
+
+def main(args=None):
+    rclpy.init(args=args)
+    cmd_vel_subscriber = CmdVelSubscriberNode()
+    try:
+        cmd_vel_subscriber.spin()
+    finally:
+        cmd_vel_subscriber.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
